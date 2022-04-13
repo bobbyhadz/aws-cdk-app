cc62323ba86b5cdab2a985b5cc46823293f5c26c
Borislav Hadzhiev <opmftw@gmail.com>
2022-04-13 10:50:17 +0300

    minor changes

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,9 +1,12 @@
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib';
 
+// 👇 Stack definition
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // 👇 using a Construct
     const bucket = new s3.Bucket(this, 'uploads-bucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
